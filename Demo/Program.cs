f13962dbca1b30469eff0b3306f09046f3825f6e
Gaber11 <gaberezzat0@gmail.com>
2024-12-06 23:31:25 +0200

    1-IF - Switch [Numeric Type 1]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,10 +4,35 @@
     {
         static void Main(string[] args)
         {
-            #region 
-
-
-
+            #region 1-IF - Switch [Numeric Type 1]
+            //A] Conditional statement [If, Switch till c# 6]
+            //Example 1 [Numeric type: Equality] [Month in first quarter]
+            //Console.WriteLine("Enter The number of month : ");
+            //int MonthNum=int.Parse(Console.ReadLine());
+            //if (MonthNum == 1)
+            //    Console.WriteLine("jeunuary");
+            //else if (MonthNum == 2)
+            //    Console.WriteLine("Fereuary");
+            //else if (MonthNum == 3) 
+            //    Console.WriteLine("March");
+            //else 
+            //    Console.WriteLine("Not in Quarter one");
+            //---> Switch
+            //switch (MonthNum)
+            //{
+            //    case 1:
+            //        Console.WriteLine("jeunuary");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Fereuary");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("March");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Not in Quarter one");
+            //        break;
+            //}
 
             #endregion
 
