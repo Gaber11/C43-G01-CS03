ca027417a0338e44b0f88837b62835bc2c544e4d
Gaber11 <gaberezzat0@gmail.com>
2024-12-07 19:21:28 +0200

    7- Write a program that allows the user to insert an integer then print a multiplication table up to 12.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -83,48 +83,57 @@
 
             #region 6- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
 
+            //Console.WriteLine("Enter Number : ");
+            //int Num =int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= Num; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+            #endregion
+
+            #region 7- Write a program that allows the user to insert an integer then print a multiplication table up to 12.
             Console.WriteLine("Enter Number : ");
-            int Num =int.Parse(Console.ReadLine());
-            for (int i = 1; i <= Num; i++)
+            int Num = int.Parse(Console.ReadLine());
+
+            for (int i =1; i <= 12; i++)
             {
-                Console.WriteLine(i);
+                int multiplication = i * Num;
+
+                Console.WriteLine(multiplication);
             }
+                #endregion
 
+                #region
+                #endregion
 
+                #region
+                #endregion
 
+                #region
+                #endregion
 
-            #endregion
+                #region
+                #endregion
 
-            #region
-            #endregion
+                #region
+                #endregion
 
-            #region
-            #endregion
+                #region
+                #endregion
 
-            #region
-            #endregion
+                #region
+                #endregion
 
-            #region
-            #endregion
+                #region
+                #endregion
 
-            #region
-            #endregion
+                #region
+                #endregion
 
-            #region
-            #endregion
-
-            #region
-            #endregion
-
-            #region
-            #endregion
-
-            #region
-            #endregion
-
-            #region
-            #endregion
-
-        }
+            }
     }
 }
