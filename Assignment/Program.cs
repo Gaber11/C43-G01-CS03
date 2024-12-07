a62c3695e587cf4dae297960df83b9e9b6f993e2
Gaber11 <gaberezzat0@gmail.com>
2024-12-07 18:15:38 +0200

    1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no

---
+++ Assignment/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1-Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no
+
+            Console.WriteLine("Enter Number");
+            int Num = int.Parse(Console.ReadLine());
+           if (Num % 4 == 0 && Num % 3 == 0)
+                Console.WriteLine("Yes");
+  
+          else
+                Console.WriteLine("No");
+           
+
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+        }
+    }
+}
